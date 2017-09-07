8134c6778b411f15c23a6a837eea82a4a9cdc1d2
tiendm2506 <minhtien2506it@gmail.com>
2017-09-07 15:49:52 +0700

    Add comment Fb

---
+++ main.js
@@ -0,0 +1,8 @@
+﻿ $(function(){
+     jQuery(".mouse").click(function() {
+         jQuery('html, body').animate({
+             scrollTop: jQuery(".knowledge").offset().top
+         }, 800);
+     });
+})  
+ 
